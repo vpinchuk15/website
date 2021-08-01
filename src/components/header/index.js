0039054c443f4a2bbbf597f603bc34352a35de63
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-07-31 20:20:20 -0700

    Header update

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './header.css'
+import Mobile from './mobile';
+import Web from './web/index'
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
     return (
-        <div>
-            This is Header
+        <div className="header">
+            <div className="logo">Vladimir Pinchuk</div>
+            <div className="menu">
+                <div className="web-menu"><Web /></div>
+                <div className="mobile-menu">
+                    <div onClick={() => setIsOpen(!isOpen)}>
+                        <i class="fi-rr-apps menu-icon"></i>
+                    </div>
+                    {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen}/>}
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Header
+
