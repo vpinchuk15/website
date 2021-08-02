e6f0ea15c437b662876587ed79c8d4c8deb4de3c
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-08-01 23:08:11 -0700

    header name change

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,7 +7,7 @@ function Header() {
     const [isOpen, setIsOpen] = useState(false)
     return (
         <div className="header">
-            <div className="logo">Vladimir Pinchuk</div>
+            <div className="logo">Vlad's folio</div>
             <div className="menu">
                 <div className="web-menu"><Web /></div>
                 <div className="mobile-menu">
