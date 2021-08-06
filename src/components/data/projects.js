d40322dbf4fa73734b7fb0d8f7802118ca85885a
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-08-05 21:14:33 -0700

    Visuals

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -3,18 +3,18 @@ export const ProjectData=[
         id:1,
         title:"Chatbot",
         about:"This is my chatbot that I could use for the time that I am not available but still want somebody to leave a message behind. This bot is trained to give basic responses to very general questions.",
-        tags:["Python", "Machine Learning", "Neural Networks", "tensorflow"],
+        tags:["Python", "Machine Learning", "Neural Networks", "tensorflow", "Chat room", "Bot"],
         demo:"",
         github:"https://github.com/vpinchuk15/chatbot",
-        image:"https://images.ctfassets.net/lzny33ho1g45/best-chatbot-builders-p-img/67ef8a7f3c4be83cecdc5569d1233da0/file.png?w=1520&fm=jpg&q=30&fit=thumb&h=760"
+        image:"https://t4.ftcdn.net/jpg/02/11/61/95/360_F_211619589_fnRk5LeZohVD1hWInMAQkWzAdyVlS5ox.jpg"
     },
     {
         id:2,
         title:"Address Book Application",
         about:"This is an address Book application that not only stores information about users but also has an administrative function abilities like adding, deleting or changing records based on permission levels.",
-        tags:["Python", "Address Book", "Penetration testing"],
+        tags:["Python", "Address Book","Contact Management","Contact List"],
         demo:"",
         github:"https://github.com/vpinchuk15/ABA",
-        image:"https://www.clipartmax.com/png/middle/93-930109_iphone-apple-laptop-iphone-contacts-icon-transparent-os-x-address-book-icon.png"
+        image:"https://i1.wp.com/www.chriswrites.com/wp-content/uploads/address_book.png?fit=256%2C256&ssl=1"
     }
 ]
