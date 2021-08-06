2d267d58a3fdf4ff6004ea4aeb9debc2861e4dc9
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-08-05 18:36:31 -0700

    Projects Update

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -3,18 +3,18 @@ export const ProjectData=[
         id:1,
         title:"Chatbot",
         about:"This is my personal assistant that fills in for the times that I am not at my computer or available!",
-        tags:["Python", "Machine Learning", "Neural Networks"],
+        tags:["Python", "Machine Learning", "Neural Networks", "tensorflow"],
         demo:"",
         github:"https://github.com/vpinchuk15/chatbot",
         image:"https://images.ctfassets.net/lzny33ho1g45/best-chatbot-builders-p-img/67ef8a7f3c4be83cecdc5569d1233da0/file.png?w=1520&fm=jpg&q=30&fit=thumb&h=760"
     },
     {
         id:2,
-        title:"Chatbot",
-        about:"This is my personal assistant that fills in for the times that I am not at my computer or available!",
-        tags:["Python", "Machine Learning", "Neural Networks"],
+        title:"Address Book Application",
+        about:"This is an address Book application that not only stores information about users but also has an administrative function abilities like adding, deleting or changing records based on permission levels.",
+        tags:["Python", "Address Book", "Penetration testing"],
         demo:"",
-        github:"https://github.com/vpinchuk15/chatbot",
-        image:"https://images.ctfassets.net/lzny33ho1g45/best-chatbot-builders-p-img/67ef8a7f3c4be83cecdc5569d1233da0/file.png?w=1520&fm=jpg&q=30&fit=thumb&h=760"
+        github:"https://github.com/vpinchuk15/ABA",
+        image:"https://www.clipartmax.com/png/middle/93-930109_iphone-apple-laptop-iphone-contacts-icon-transparent-os-x-address-book-icon.png"
     }
 ]
