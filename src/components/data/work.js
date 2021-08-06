765d70759d1f5ccabc357995494cb4ece9761bb4
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-08-05 18:21:11 -0700

    Text + icon

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -5,7 +5,7 @@ export const WorkData=[
         dateJoining:"06-01-2020",
         dateEnd:"08-14-2020",
         companyLogo:"https://ayaliving.com/wp-content/uploads/aya_logo.png",
-        work:"Testss sadhkj gas sadhkjgas sadh kjgas sad hkjg as sadhkjg as s adhkjgas sadhkjgas"
+        work:"Deployed a database in FileMaker that used CDML scripts to also create a GUI to operate said database to improve work efficiency."
     },
     {
         company:"TheRealReal",
@@ -13,6 +13,6 @@ export const WorkData=[
         dateJoining:"06-01-2021",
         dateEnd:"08-06-2021",
         companyLogo:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/The_RealReal_logo.svg/1280px-The_RealReal_logo.svg.png",
-        work:"fsdfsd ffsdf sdf fsdfsdff sdfsdf fsdfsdffs dfsd ffsdfsdffsd fsdffsdf sdffsd fsdf"
+        work:"Automated the reporting of vulnerability tickets from security tools to Jira then to PagerDuty while utilizing REST APIs and AWS services. Worked on remediation of critical vulnerabilities and implemented CLI plugins to the CI/CD pipeline to prevent future vulnerabilities."
     }
 ]
